ef7b3aeb710fc6e496e6c7a194f9b96da5ca042f
Arvin Mahmoudi <mahmoudi.arvin@gmail.com>
2020-05-11 01:52:26 -0400

    adding NoMatch page to the App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,21 @@ import React from 'react';
 import {BrowserRouter as Router,Route} from "react-router-dom";
 import Saved from './Pages/SavedPage';
 import Search from './Pages/SearchPage';
+import Nav from './components/Navbar/Navbar';
 
 function App() {
   return (
    
-      <Router>
-          <Route path="/" exact component={Search} />
-          <Route path="/search" exact component={Search} />
-          <Route path='/saved' exact component={Saved}/>
-      </Router>
+    <Router>
+      <div>
+        <Nav />
+          <Switch>
+            <Route path="/" exact component={Search} />
+           <Route path="/search" exact component={Search} />
+           <Route path='/saved' exact component={Saved}/>
+          </Switch>
+      </div>
+    </Router>
    
 
   );
