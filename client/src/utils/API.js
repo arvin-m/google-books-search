5be79efd0f0d7d103d064f59c78111a480d306d5
Arvin Mahmoudi <mahmoudi.arvin@gmail.com>
2020-05-11 01:37:23 -0400

    adding API route for front-end

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,8 +1,21 @@
-import Axios from 'axios';
-const API = {
+import axios from "axios";
+
+export default {
+  // Gets books from the Google API
+  getBooks: function(q) {
+    return axios.get("/api/google", { params: { q: "title:" + q } });
+  },
+  // Gets all saved books
+  getSavedBooks: function() {
+    return axios.get("/api/books");
+  },
+  // Saves a book to the database
+  saveBook: function(bookData) {
+    return axios.post("/api/books", bookData);
+  },
+  // Delete  book with  id
+  deleteBook: function(id) {
+    return axios.delete("/api/books/" + id);
+  }
   
-}
-  
-  
-  export default API;
-  
+};
